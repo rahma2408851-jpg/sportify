c11c0e1ca504c51f1d9015353c53c107dbc8fafd
nour2400429-prog <nour2400429@miuegypt.edu.eg>
2026-08-06 17:53:21 +0300

    Implement auto-dismiss for flash messages
    Add functionality to fade out flash messages after 4 seconds.

--- public /Admin /JS /admin.js
+++ public /Admin /JS /admin.js
@@ -1,1 +1,14 @@
+(function () {
+  "use strict";
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".flash-message").forEach(function (msg) {
+      setTimeout(function () {
+        msg.style.transition = "opacity .4s ease";
+        msg.style.opacity = "0";
+        setTimeout(() => msg.remove(), 400);
+      }, 4000);
+    });
+  });
+})();
 
