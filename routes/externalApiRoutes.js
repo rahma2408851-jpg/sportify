d609d909c113de18d92c3c4e785f95166bf6392b
nour2400429-prog <nour2400429@miuegypt.edu.eg>
2026-08-06 17:28:05 +0300

    Delete routes/externalApiRoutes.js

--- routes/externalApiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const externalApiController = require("../controllers/externalApiController");
-
-router.get("/currency", externalApiController.getCurrencyRates);
-router.get("/info", externalApiController.getExternalInfo);
-
-module.exports = router;
